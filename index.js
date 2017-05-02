a50cfe9e9bf8ced8b3bfd6fbf45e7e5eef3f14b4
Ian Follett <ianfollett@gmail.com>
2017-05-01 22:04:34 -0700

    Sorry, I didn’t commit these files to my GitHub repository before I submitted the assignment. I’m still getting the hang of it. Will do better next time.

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
-/**
- * Created by ianfollett on 4/17/17.
- */
+'use strict'
+
+let http = require("http"), fs = require('fs'), qs = require("querystring");
+let movie = require("./lib/movie.js");
+
+function serveStatic(res, path, contentType, responseCode){
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data){
+        if(err){
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+        }
+        else{
+            res.writeHead(responseCode, {'Content-Type': contentType});
+            res.end(data);
+        }
+    });
+}
+
+http.createServer((req,res) => {
+    let url = req.url.split("?");  // seperate route from query string
+    let params = qs.parse(url[1]); // convert query string to object
+    let path = url[0].toLowerCase();
+
+    switch(path) {
+        case '/':
+            serveStatic(res, '/../public/home.html', 'text/html');
+            break;
+
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('About');
+            break;
+
+        case '/get':
+            let found = movie.get(params.title); // get movie object
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let results = (found) ? JSON.stringify(found) : "Not found";
+            res.end('searching for ' + params.title + "\n" + results);
+            break;
+
+        case '/delete':
+            movie.delete(params.title);
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end(params.title + "\n" + "has been deleted.");
+            break;
+
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('404:Page not found.');
+    }
+
+}).listen(process.env.PORT || 3000);
